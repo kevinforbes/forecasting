4a01da8b0160ea95b8c017360a9c5ec38bc421a3
kevinforbes <kevinforbes@gmail.com>
2013-11-05 21:40:21 -0500

    first commit

---
+++ Forecasting/ForecastingEngine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Forecasting
+{
+    public class ForecastingEngine
+    {
+
+        public DeviceForecast CalculateDeviceConsumableForecast(List<MonitoringMessage> messages, Guid deviceId)
+        {
+            DeviceForecast forecast = new DeviceForecast();
+
+            foreach (MonitoringMessage message
+                in messages.OrderByDescending(x => x.LastReportedAt))
+            {
+
+                //if level is greater than last reported, they probably replaced the cartridge
+
+                //calculate average daily use, keep in mind it might not have been reported every day (some days may have been skipped)
+
+                //calculate Days remaining until ten percent left
+
+                //if level is 0, stop counting usage towards average
+            }
+
+            return forecast;
+        }
+    }
+
+    public class DeviceForecast
+    {
+        public string DeviceId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public List<ConsumableForecast> ConsumableForecasts { get; set; }
+    }
+
+    public class ConsumableForecast
+    {
+        public string ConsumableId { get; set; }
+        public int AverageDailyUse { get; set; }
+        public int DaysRemainingToTenPercent { get; set; }
+        public int CurrentLevel { get; set; }
+    }
+
+    public class MonitoringMessage
+    {
+        public Guid DeviceId { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid ManufacturerId { get; set; }
+        public Guid DealerId { get; set; }
+        public string Name { get; set; }
+        public DateTime LastReportedAt { get; set; }
+        public DateTime FirstReportedAt { get; set; }
+        public virtual ICollection<Consumable> Consumables { get; set; }
+
+
+        public string ProviderDeviceId { get; set; }
+        public string ProviderCustomerId { get; set; }
+        public string DeviceModelNumber { get; set; }
+        public string Manufacturer { get; set; }
+        public string MacAddress { get; set; }
+        public string IpAddress { get; set; }
+        public string CustomerName { get; set; }
+        public Guid GroupId { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; } //ex:network
+        public string ManagementStatus { get; set; } //ex:Managed
+        public string LicenseStatus { get; set; } //ex:Full
+        public string AssetNumber { get; set; }
+        public string SerialNumber { get; set; }
+        public string Location { get; set; }
+        public string DeviceDescription { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+
+
+    public class Consumable //ex:PrintFleet calls this ColorSupplies
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } //ex:Cyan, Magenta, Black
+        public string Label { get; set; } //ex:TonerLevel_Black, TonerLevel_Cyan
+        public string Color { get; set; }
+        public string OEMPartNumber { get; set; }
+        public string Type { get; set; } //ex:Toner, Drum
+        public int Level { get; set; }
+    }
+
+}
---
+++ Forecasting/ForecastingTests.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Forecasting
+{
+    [TestClass]
+    public class ForecastingTests
+    {
+
+        [TestMethod]
+        public void Given30Messages1DayApart_StartingAt100AndDecreasingBy1EachDayFor29Days_AverageDailyUseEquals1()
+        {
+            Guid customerId = Guid.NewGuid();
+            Guid deviceId = Guid.NewGuid();
+
+            List<MonitoringMessage> messages =
+                new MonitoringMessageListBuilder(customerId, deviceId).AListOfMessages(29, 1, 100, 1).Build();
+
+            ForecastingEngine engine = new ForecastingEngine();
+            DeviceForecast forecast = engine.CalculateDeviceConsumableForecast(messages, deviceId);
+
+            // 4 consumables for this device, all are decreasing at the same rate of 1 percent per day 
+            // which means they have 50 days left until hitting 10 percent
+            Assert.AreEqual(4, forecast.ConsumableForecasts.Count);
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.CurrentLevel != 61));
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.AverageDailyUse != 1));
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.DaysRemainingToTenPercent != 50));
+        }
+
+        [TestMethod]
+        public void Given30Messages2DaysApart_StartingAt100AndDecreasingBy2BetweenEachMessage_DaysLeftEquals59()
+        {
+            Guid customerId = Guid.NewGuid();
+            Guid deviceId = Guid.NewGuid();
+
+            List<MonitoringMessage> messages =
+                new MonitoringMessageListBuilder(customerId, deviceId).AListOfMessages(29, 1, 100, 1).Build();
+
+            ForecastingEngine engine = new ForecastingEngine();
+            DeviceForecast forecast = engine.CalculateDeviceConsumableForecast(messages, deviceId);
+
+            // 4 consumables for this device, all are decreasing at the same rate of 1 percent per day 
+            // which means they have 50 days left until hitting 10 percent
+            Assert.AreEqual(4, forecast.ConsumableForecasts.Count);
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.CurrentLevel != 61));
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.AverageDailyUse != 1));
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.DaysRemainingToTenPercent != 50));
+        }
+
+        public void Given2Messages30DaysApart_StartingAt100AndDecreasingBy30BetweenMessages_AverageDailyUseEquals1()
+        {
+            Guid customerId = Guid.NewGuid();
+            Guid deviceId = Guid.NewGuid();
+
+            List<MonitoringMessage> messages =
+                new MonitoringMessageListBuilder(customerId, deviceId).AListOfMessages(2, 30, 100, 29).Build();
+
+
+            ForecastingEngine engine = new ForecastingEngine();
+            DeviceForecast forecast = engine.CalculateDeviceConsumableForecast(messages, deviceId);
+
+
+            // 4 consumables for this device, all are decreasing at the same rate of 1 percent per day 
+            // which means they have 50 days left until hitting 10 percent
+            Assert.AreEqual(4, forecast.ConsumableForecasts.Count);
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.CurrentLevel != 61));
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.AverageDailyUse != 1));
+            Assert.IsFalse(forecast.ConsumableForecasts.Any(x => x.DaysRemainingToTenPercent != 50));
+        }
+
+    }
+
+    public class MonitoringMessageListBuilder
+    {
+        private readonly Guid _customerId;
+        private readonly Guid _deviceId;
+        private List<MonitoringMessage> _messages;
+
+        public MonitoringMessageListBuilder(Guid customerId, Guid deviceId)
+        {
+            _customerId = customerId;
+            _deviceId = deviceId;
+            _messages = new List<MonitoringMessage>();
+        }
+
+        public MonitoringMessageListBuilder AListOfMessages(int numberOfMessages, int daysApart, int startingPercentage,
+            int decreaseByPerMessage)
+        {
+            DateTime lastDateUsed = DateTime.Today;
+            int lastPercentageUsed = startingPercentage;
+
+            for (int i = 0; i < numberOfMessages; i++)
+            {
+                DateTime dateToUse = lastDateUsed.AddDays(daysApart);
+                int percentage = lastPercentageUsed - decreaseByPerMessage;
+
+                _messages.Add(
+                    new MonitoringMessageBuilder(_customerId, dateToUse)
+                        .WithCyanMagentaYellowAndBlackConsumables(percentage, percentage, percentage, percentage)
+                        .Build());
+
+                lastDateUsed = dateToUse;
+                lastPercentageUsed = percentage;
+            }
+
+            return this;
+        }
+
+        public List<MonitoringMessage> Build()
+        {
+            return _messages;
+        }
+    }
+
+    public class MonitoringMessageBuilder
+    {
+        private readonly MonitoringMessage _message;
+
+        public MonitoringMessageBuilder(Guid customerId, DateTime reportedDate)
+        {
+            _message = new MonitoringMessage
+            {
+                CustomerId = customerId,
+                ProviderDeviceId = Guid.NewGuid().ToString(),
+                DealerId = Guid.NewGuid(),
+                DeviceDescription = "Brother MFC 8two9",
+                DeviceModelNumber = "MFC829",
+                LastReportedAt = reportedDate,
+                TimeStamp = DateTime.UtcNow
+            };
+        }
+
+        public MonitoringMessageBuilder WithConsumable(string color, int level)
+        {
+            _message.Consumables.Add(
+                new Consumable
+                {
+                    Color = color,
+                    Level = level,
+                    Id = Guid.NewGuid().ToString(),
+                    Type = "Toner"
+                });
+
+            return this;
+        }
+
+        public MonitoringMessageBuilder WithCyanMagentaYellowAndBlackConsumables(int cyanLevel, int magentaLevel,
+            int yellowLevel, int blackLevel)
+        {
+            _message.Consumables.Add(
+                new Consumable
+                {
+                    Color = System.Drawing.Color.Cyan.ToString(),
+                    Level = cyanLevel,
+                    Id = Guid.NewGuid().ToString(),
+                    Type = "Toner"
+                });
+
+            _message.Consumables.Add(
+                new Consumable
+                {
+                    Color = System.Drawing.Color.Magenta.ToString(),
+                    Level = magentaLevel,
+                    Id = Guid.NewGuid().ToString(),
+                    Type = "Toner"
+                });
+
+            _message.Consumables.Add(
+                new Consumable
+                {
+                    Color = System.Drawing.Color.Yellow.ToString(),
+                    Level = yellowLevel,
+                    Id = Guid.NewGuid().ToString(),
+                    Type = "Toner"
+                });
+
+            _message.Consumables.Add(
+                new Consumable
+                {
+                    Color = System.Drawing.Color.Black.ToString(),
+                    Level = blackLevel,
+                    Id = Guid.NewGuid().ToString(),
+                    Type = "Toner"
+                });
+
+            return this;
+        }
+
+        public MonitoringMessage Build()
+        {
+            return _message;
+        }
+    }
+}
